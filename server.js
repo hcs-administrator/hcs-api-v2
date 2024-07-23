d3c7abe31534fce2a871ed1f595a69d4935ed916
Jeff Kranenburg <jwkranenburg@icloud.com>
2024-07-23 20:21:54 +1200

    server setup complete - routes in own branches

---
+++ server.js
@@ -0,0 +1,57 @@
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+
+// SWAGGER UI
+const swaggerJSDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+const { SwaggerTheme, SwaggerThemeNameEnum } = require('swagger-themes');
+const YAML = require('yamljs');
+const basicAuth = require('express-basic-auth');
+
+// Instances
+const app = express()
+const theme = new SwaggerTheme();
+const swaggerDefinition = YAML.load('./swagger.yaml');
+
+// Variables
+const port = process.env.PORT || 3000
+
+// Middleware (Security)
+app.use(cors()) //This needs to be secured
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    next();
+});
+
+// Swagger Themes
+const optionsV1 = {
+    explorer: true,
+    customCss: theme.getBuffer(SwaggerThemeNameEnum.DARK)
+};
+  
+const optionsV2 = {
+    explorer: false,
+    customCss: theme.getBuffer(SwaggerThemeNameEnum.MUTED)
+}
+
+// Swagger Docs
+const options = {
+    swaggerDefinition,
+    apis: ['./routes/**/*.js'],
+};
+  
+const swaggerSpec = swaggerJSDoc(options);
+
+app.use('/_dark', basicAuth({
+    users: { '22222' : 'x-api' },
+    challenge: true,
+}), swaggerUi.serve, swaggerUi.setup(swaggerSpec, optionsV1));
+
+app.use('/_light', basicAuth({
+  users: { '22222' : 'x-api' },
+  challenge: true,
+}), swaggerUi.serve, swaggerUi.setup(swaggerSpec, optionsV2));
+
+app.listen(port, () => console.log(`App listening on port ${port}!`));
