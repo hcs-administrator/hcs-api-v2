8ae3f400f648e8b340d5bf24548c5f99b4214fc1
Jeff Kranenburg <jwkranenburg@icloud.com>
2025-03-17 21:25:58 +1300

    temp

--- __functions/verify.js
+++ __functions/verify.js
@@ -7,8 +7,10 @@ const jwt = require('jsonwebtoken');
 const verify = (req, res, next) => {
 
     if( Object.keys(req.body).length <= 0) {
+        
         res.status(400).json({"ERROR" : "Missing Token"})
         return;
+
     } else {
 
         if (req.body.token === "SUPER_ADMIN") {
