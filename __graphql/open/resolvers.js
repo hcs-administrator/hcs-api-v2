6964ce316993dd058be113b1cc37f23c9dfe91ff
Jeff Kranenburg <jwkranenburg@icloud.com>
2025-04-07 10:51:44 +1200

    added comments

--- __graphql/open/resolvers.js
+++ __graphql/open/resolvers.js
@@ -25,8 +25,11 @@ const getTest = async () => {
 
 const getToken = async ({ eid, password }) => {
 
+    // EXAMPLE COMMENTS ONLY
+    // URL = the REST BASED URL
     let url = `${process.env.BASE_URL}/token/generate`
 
+    // CONFIG = this data really matches a normal REST request. Basically a copy from POSTMAN
     let config = {
         method: 'POST',
         url: url,
@@ -42,6 +45,8 @@ const getToken = async ({ eid, password }) => {
 
     return await axios(config)
     .then(resp => {
+        //Return from REST request, but with the property that requested.
+        //This query is redundant to the REST request, however when it comes to multiple requests, this will help
         return { "token" : resp.data }
     })
     .catch(err => console.log(err))
